82453d8e2ce40d15a063b9163eee308b09b87091
aarbelaez15 <alejoarbe0@gmail.com>
2025-11-18 09:25:11 -0500

    Prueba despliegue CD

--- co/edu/uco/retnova/infrastructure/config/database.py
+++ co/edu/uco/retnova/infrastructure/config/database.py
@@ -1,3 +1,4 @@
+import os
 import psycopg2
 from psycopg2 import pool
 
@@ -5,13 +6,27 @@ class DatabaseConfig:
     """Configura y gestiona el pool de conexiones a PostgreSQL."""
 
     def __init__(self):
+
+        # ============================
+        #  CONFIGURACIÓN HÍBRIDA
+        # ============================
+        # Railway -> usa variables de entorno
+        # Local   -> usa valores por defecto
+
+        db_user = os.getenv("PGUSER", "postgres")
+        db_password = os.getenv("PGPASSWORD", "1234")
+        db_host = os.getenv("PGHOST", "localhost")
+        db_port = os.getenv("PGPORT", "5432")
+        db_name = os.getenv("PGDATABASE", "retnova_db")
+
+        # Crear pool de conexiones
         self.connection_pool = pool.SimpleConnectionPool(
             1, 10,
-            user="postgres",
-            password="1234",
-            host="localhost",
-            port="5432",
-            database="retnova_db"
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+            database=db_name
         )
 
     def get_connection(self):
